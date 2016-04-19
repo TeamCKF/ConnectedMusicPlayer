dfe66a94a44c420cd620754e89d60f389840c9e1
gomel_f <gomel_f@epitech.eu>
2016-04-19 18:23:54 +0200

    server

--- server/main.c
+++ server/main.c
@@ -5,7 +5,7 @@
 ** Login   <gomel_f@epitech.net>
 **
 ** Started on  Tue Apr 19 15:37:59 2016 Frédéric GOMEL
-** Last update Tue Apr 19 17:48:06 2016 guillaume
+** Last update Tue Apr 19 18:22:13 2016 Frédéric GOMEL
 */
 
 #include <fmod.h>
@@ -21,9 +21,9 @@ char *path_music;
 int  lang;
 int  port;
 
-void	credit_display()
+void	copyright_display()
 {
-  printf("\033[05m\033[33m==========================================\n");
+  printf("\033[33m==========================================\n");
   printf("\033[31m CMP-Server.  Copyright (c) TeamCKF 2016.\n");
   printf("\033[33m==========================================\n");
 }
@@ -82,16 +82,91 @@ void	create_config()
   write(fd, portp, strlen(portp));
 }
 
+int	equal_char(char *src, char *comp)
+{
+  int	i;
+
+  i = 0;
+  while (src[i] && comp[i])
+    {
+      if (src[i] == comp[i])
+	i++;
+      else
+	return (0);
+    }
+  if (src[i] == '\0')
+    return (1);
+  return (0);
+}
+
+int	lenght(char *str)
+{
+  int	i;
+  int	j;
+
+  i = (j = 0);
+  while (str[i] != '=')
+    i++;
+  if (str[i] == '=')
+    i++;
+  while (str[i] != '\0')
+    {
+      i++;
+      j++;
+    }
+  return j;
+}
+
 void	get_path(int fd)
 {
+  char	*para;
+  char	*portp;
+  int	i;
+  int	j;
 
+  i = (j = 0);
+  para = gnl(fd);
+  if (equal_char(para, "LANG=Fr_fr") == 1)
+    lang = 1;
+  else
+    lang = 2;
+  para = gnl(fd);
+  if ((path_music = malloc(sizeof(char) * lenght(para))) == NULL)
+    exit(EXIT_FAILURE);
+  while (para[i] != '=')
+    i++;
+  if (para[i] == '=')
+    i++;
+  while (para[i] != '\0')
+    path_music[j++] = para[i++];
+  para = gnl(fd);
+  if ((portp = malloc(sizeof(char) * lenght(para))) == NULL)
+    exit(EXIT_FAILURE);
+  i = (j = 0);
+  while (para[i] != '=')
+    i++;
+  if (para[i] == '=')
+    i++;
+  while (para[i] != '\0')
+    portp[j++] = para[i++];
+  port = atoi(portp);
+  if (lang == 1)
+    {
+      printf("CHEMIN MUSIQUE : %s\n", path_music);
+      printf("PORT : %i\n", port);
+    }
+  else
+    {
+      printf("PATH MUSIC : %s\n", path_music);
+      printf("PORT : %i\n", port);
+    }
 }
 
 int	main(int ac, char **av)
 {
   int fd;
 
-  credit_display();
+  copyright_display();
   if ((fd = open("config.cfg", O_RDONLY)) == -1)
     create_config();
   else
